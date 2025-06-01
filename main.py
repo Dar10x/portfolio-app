5c77280e1ede350c65ea1c4c3fbfcea13b1c7f23
Dario <darioari200521@gmail.com>
2025-06-01 13:20:12 -0500

    Add button links and an empty column

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas
 
-st.set_page_config(layout="centered",page_title="Nestor's Portfolio")
-col1,col2 = st.columns(2,vertical_alignment="center")
+st.set_page_config(layout="wide",page_title="Nestor's Portfolio")
+col1,col2 = st.columns(2,vertical_alignment="top")
 
 with col1:
-    st.image("images/photo.png",width=200,use_container_width="auto")
+    st.image("images/photo.png",width=200)
+
 
 with col2:
     st.title("Nestor Ari")
@@ -20,13 +21,19 @@ content2 = """
 Below you can find my python projects. 
 """
 st.subheader(content2)
-st.write("Feel free to use wichever you want")
+st.write("Feel free to use whichever you want")
 
-col3,col4 = st.columns(2)
+col3,empty_col,col4 = st.columns([1.5,0.3,1.5])
 df = pandas.read_csv("data.csv",sep=";")
 with col3:
     for index,row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + f"{index+1}.png")
+        st.link_button(label='Link',url=row["url"])
 with col4:
     for index,row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + f"{index+1}.png")
+        st.link_button(label='Link', url=row["url"])
