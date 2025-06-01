7a132bb6d6c5a8c15ca5b65ea5b09adc7a0180a3
Dario <darioari200521@gmail.com>
2025-06-01 12:10:13 -0500

    Add description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(layout="centered")
+st.set_page_config(layout="centered",page_title="Nestor's Portfolio")
 col1,col2 = st.columns(2,vertical_alignment="center")
 
 with col1:
@@ -11,6 +11,12 @@ with col2:
     content = """
     Hi, I am Nestor! Right now I am studying at Pontificia Universidad Catolica del Peru. I've been working with python since 2025.
     I also know how to code in C and C++. I have an advance level of english and can also work with data in SQL. 
-    Here are my projects I have been working on
+    Here are my projects I have been working on.
     """
-    st.write(content,width=720)
+    st.write(content)
+
+content2 = """
+Below you can find my python projects. 
+"""
+st.subheader(content2)
+st.write("Feel free to use wichever you want")
