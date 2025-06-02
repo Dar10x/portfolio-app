00ddf18e98feb8259bca98e12ce98adb58dfed1c
Dario <darioari200521@gmail.com>
2025-06-02 14:52:00 -0500

    Use getenv to keep it safer

--- send_emails.py
+++ send_emails.py
@@ -2,11 +2,11 @@ import smtplib, ssl,os
 
 
 def send_email(message):
-    host = "smtp.gmail.com"
+    host = os.getenv("HOST")
     port = 465
-    username = "darioari200521@gmail.com"
+    username = os.getenv("USERNAME")
     password = os.getenv("PASSWORD")
-    receiver = "darioari200521@gmail.com"
+    receiver = os.getenv("USERNAME")
     my_context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=my_context) as server:
         server.login(username, password)
