5aa4c7874e0155dd526b710a624c114c6b76b9e0
Dario <darioari200521@gmail.com>
2025-06-02 15:25:36 -0500

    Encode the username and password

--- send_emails.py
+++ send_emails.py
@@ -1,12 +1,13 @@
 import smtplib, ssl,os
+import streamlit as st
 
 
 def send_email(message):
-    host = os.getenv("HOST")
+    host = st.secrets["database"]["host"]
     port = 465
-    username = os.getenv("USERNAME")
-    password = os.getenv("PASSWORD")
-    receiver = os.getenv("USERNAME")
+    username = st.secrets["database"]["username"]
+    password = st.secrets["database"]["password"]
+    receiver = st.secrets["database"]["receiver"]
     my_context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=my_context) as server:
         server.login(username, password)
